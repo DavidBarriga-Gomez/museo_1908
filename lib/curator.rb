a1917baf0d26a423deae9d11a61ecd15b3407ac4
David Gomez <davdg25@gmail.com>
2019-09-17 09:54:15 -0600

    finished iteration2 setup

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,10 +1,36 @@
 class Curator
-  attr_reader :photographs
+  attr_reader :photographs, :artists
   def initialize
     @photographs = []
+    @artists = []
   end
 
   def add_photograph(photgraph)
     @photographs.push photgraph
+  end
+
+  def add_artist(artist)
+    @artists.push artist
+  end
+
+  def find_artist_by_id(id)
+    artist_by_id = nil
+    @artists.each do |artist|
+      if artist.id == id
+        artist_by_id = artist
+      end
+    end
+    artist_by_id
+  end
+
+  def find_photograph_by_id(id)
+    photograph_by_id = nil
+    @photographs.each do |photo|
+      if photo.id == id
+        photograph_by_id = photo
+        # binding.pry
+      end
+    end
+    photograph_by_id
   end
 end
